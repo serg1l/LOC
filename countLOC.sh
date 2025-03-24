945fc0a8082865c5bdae42a44a78d30e31557b6f
sergil <98274513+serg1l@users.noreply.github.com>
2025-03-23 22:13:24 -0500

    first commit

---
+++ countLOC.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+files="${1:-$(pwd)}"
+count=0
+
+process_dir(){
+    local current="${1}"
+
+    for file in "$current"/*; do
+        if [ -f "$file" ]; then
+            lines=$(bat < "$file" | wc -l)
+            count=$((count + lines))
+	    echo "$file lines: $lines"
+        elif [ -d "$file" ]; then
+            process_dir "$file"
+        fi
+    done
+}
+
+process_dir "$files"
+
+echo "---------------"
+echo "total: $count"
